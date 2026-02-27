8f0fc559b0277a808ce1749de5b6b63291646232
손승범 <sonseungbeom@sonseungbeom-ui-MacBookPro.local>
2026-02-27 23:44:06 +0900

    api 주소 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         <Script
-          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
           strategy="beforeInteractive"
         />
         <AuthProvider>
